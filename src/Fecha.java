d66504658f2506066064deef15051861e6a74975
dima_ <dima_@DESKTOP-TDCVK0F>
2017-02-26 12:50:34 +0100

    Creacion método diasMes y su implementacion en valida

--- src/Fecha.java
+++ src/Fecha.java
@@ -11,31 +11,6 @@ public class Fecha {
 		this.anio = anio;
 	}
 
-	private int diasMes ( ) {
-		int diasMes = 0;
-		switch (mes) {
-		case 1:
-		case 3:
-		case 5:
-		case 7:
-		case 8:
-		case 10:
-		case 12: diasMes = 31;
-		break;
-		case 4:
-		case 6:
-		case 9:
-		case 11 : diasMes = 30;
-		break;
-		case 2 :
-		if ( (anio % 400 == 0) || ( (anio % 4 == 0) && (anio % 100!= 0) ) )
-			diasMes = 29;
-		else diasMes = 28;
-		break;
-		}
-		return diasMes;
-	}
-
 	public boolean valida() {
 		if (dia < 1 || dia > 31)
 			return false;
@@ -50,5 +25,32 @@ public class Fecha {
 	}
 
 	// determinamos la cantidad de días del mes:
+	private int diasMes() {
+		int diasMes = 0;
+		switch (mes) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			diasMes = 31;
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			diasMes = 30;
+			break;
+		case 2:
+			if ((anio % 400 == 0) || ((anio % 4 == 0) && (anio % 100 != 0)))
+				diasMes = 29;
+			else
+				diasMes = 28;
+			break;
+		}
+		return diasMes;
+	}
 
 }
