b46f9ae4451576717d3bc8b61ce4f18fc54bbd08
dima_ <dima_@DESKTOP-TDCVK0F>
2017-02-26 12:46:20 +0100

    Primera refactorización: creación del método DiasMes y sustitucion el el método valida

--- src/Fecha.java
+++ src/Fecha.java
@@ -11,15 +11,7 @@ public class Fecha {
 		this.anio = anio;
 	}
 
-	public boolean valida() {
-		if (dia < 1 || dia > 31)
-			return false;
-		if (mes < 1 || mes > 12)
-			return false;
-		if (anio < 0)
-			return false;
-
-		// determinamos la cantidad de días del mes:
+	private int diasMes ( ) {
 		int diasMes = 0;
 		switch (mes) {
 		case 1:
@@ -28,27 +20,35 @@ public class Fecha {
 		case 7:
 		case 8:
 		case 10:
-		case 12:
-			diasMes = 31;
-			break;
+		case 12: diasMes = 31;
+		break;
 		case 4:
 		case 6:
 		case 9:
-		case 11:
-			diasMes = 30;
-			break;
-		case 2: // verificación de año bisiesto
-			if ((anio % 400 == 0) || ((anio % 4 == 0) && (anio % 100 != 0)))
-				diasMes = 29;
-			else
-				diasMes = 28;
-			break;
+		case 11 : diasMes = 30;
+		break;
+		case 2 :
+		if ( (anio % 400 == 0) || ( (anio % 4 == 0) && (anio % 100!= 0) ) )
+			diasMes = 29;
+		else diasMes = 28;
+		break;
 		}
-		if (dia > diasMes)
+		return diasMes;
+	}
+
+	public boolean valida() {
+		if (dia < 1 || dia > 31)
+			return false;
+		if (mes < 1 || mes > 12)
+			return false;
+		if (anio < 0)
+			return false;
+		if (dia > diasMes())
 			return false;
 		else
 			return true;
 	}
 
+	// determinamos la cantidad de días del mes:
 
 }
